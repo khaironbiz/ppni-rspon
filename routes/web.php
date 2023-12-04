2db61f5c850f0a4ad2334c81e30701ae4031ca96
telehealthindonesia <firebase.tsi@gmail.com>
2023-12-04 11:31:14 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Web\Landing\ClassController::class,'index'])->name('landing.home');
+Route::get('/', [App\Http\Controllers\Web\LandingController::class,'index'])->name('landing.nihss');
 
 @include('web/auth.php');
 
