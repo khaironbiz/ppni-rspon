043e97207635d028f11499a3dee33edc2b318be2
khaironbiz <khaironbiz@gmail.com>
2023-12-06 23:04:28 +0700

    mr-2 wrapper

--- resources/views/admin/training_question/show.blade.php
+++ resources/views/admin/training_question/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.admin')
 @section('content')
     @if(session('success'))
-        <div class="alert alert-success ml-2">
+        <div class="alert alert-success mr-2 ml-2">
             {{ session('success') }}
         </div>
     @elseif(session('danger'))
-        <div class="alert alert-danger ml-2">
+        <div class="alert alert-danger mr-2 ml-2">
             {{ session('danger') }}
         </div>
     @endif
-    <div class="card ml-2">
+    <div class="card mr-2 ml-2">
         <div class="card-header">
             <b>{{ $title }}</b>
         </div>
@@ -92,7 +92,7 @@
             </div>
         </div>
     </div>
-    <div class="card ml-2">
+    <div class="card mr-2 ml-2">
         <div class="card-header">
             <b>Soal</b>
         </div>
