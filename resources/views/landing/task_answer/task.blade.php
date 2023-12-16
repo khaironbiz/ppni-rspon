7afa7274e7bb427fe0f561b1b552283201abd7ba
khaironbiz <khaironbiz@gmail.com>
2023-12-17 07:10:25 +0800

    persetujuan mengumpulkan jawaban

--- resources/views/landing/task_answer/task.blade.php
+++ resources/views/landing/task_answer/task.blade.php
@@ -42,7 +42,7 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Persetujuan Mengumpulkan Lembar Jawaban</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <form action="{{ route('landing.task.finish') }}" method="post">
