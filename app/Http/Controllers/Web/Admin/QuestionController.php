97875463b18fdb7b5483aeefbce8ba0228dd91d9
khaironbiz <khaironbiz@gmail.com>
2023-12-18 10:44:54 +0700

    update video question dan update task detail

--- app/Http/Controllers/Web/Admin/QuestionController.php
+++ app/Http/Controllers/Web/Admin/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Question;
+use App\Models\TaskAnswerDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
@@ -41,9 +42,13 @@ class QuestionController extends Controller
     public function update(Request $request){
         $data = $request->all();
         $question = Question::find($request->id);
+        $task_answer_detail = TaskAnswerDetail::where('question_id',$question->id);
 //        dd($data);
         $update = $question->update($data);
         if($update ){
+            $update_answer_detail = $task_answer_detail->update([
+                'youtube_id_video' => $request->youtube_id_video
+            ]);
             Session::flash('success', 'Soal berhasil diupdate');
         }else{
             Session::flash('danger', 'Soal gagal diupdate');
